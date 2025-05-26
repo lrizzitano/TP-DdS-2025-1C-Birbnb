2742821e9f75106f7ae1cd140b47e4e8ca2af8f9
lrizzitano <lrizzitano@frba.utn.edu.ar>
2025-05-25 21:22:22 -0300

    agrego valor default para la coneccion de la DB

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export class MongoDBClient {
     static async connect() {
         try {
-            const conn = await mongoose.connect(process.env.MONGODB_URI + "/" + process.env.MONGODB_DB_NAME);
+            const conn = await mongoose.connect(process.env.MONGODB_URI + "/" + process.env.MONGODB_DB_NAME || 'mongodb://localhost:27017/birbnb');
             console.log(`Conectado a MongoDB: ${conn.connection.host}`);
         } catch (error) {
             console.error(`Error al conectar a MongoDB: ${error.message}`);
