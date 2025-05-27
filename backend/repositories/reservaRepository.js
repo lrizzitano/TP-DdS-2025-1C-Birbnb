f08fbbd3c573cbe803054d44b722842aa1d2ee9a
Lautaro <lautaromelchiori01@gmail.com>
2025-05-27 15:10:08 -0300

    expongo en repository metodos para traer reservas con diferentes campos populados segun la necesidad + crear y updatear reservas

--- backend/repositories/reservaRepository.js
+++ backend/repositories/reservaRepository.js
@@ -9,21 +9,33 @@ export class ReservaRepository {
         return await this.model.find();
     }
 
+    async findByIdConAlojamientoPopulado(id) {
+        return await this.model.findById(id).populate('alojamiento');
+    }
+
+    async findByIdConAlojamientoYHuespedPopulado() {
+      return await this.model.findById(id).populate('alojamiento').populate('huespedReservador');   
+    }
+
+    async findByIdConAlojamientoYAnfitrionPopulado() {
+      return await this.model.findById(id).populate('alojamiento').populate('alojamiento.anfitrion');   
+    }
+
     async findById(id) {
-        return await this.model.findById(id);
+      return await this.model.findById(id);
     }
 
     async findByAlojamiento(alojamientoId) {
-      return await this.model.find({alojamiento : alojamientoId})
+      return await this.model.find({alojamiento : alojamientoId});
     }
 
     async findByUsuario(usuarioId) {
-      return await this.model.find({huespuedReservador : usuarioId})
+      return await this.model.find({huespuedReservador : usuarioId});
     }
 
     async save(reserva) {
         if (reserva.id) { // si tiene id es pq ya existe => lo actualizo
-          return await this.model.findByIdAndUpdate(reserva.id, reserva, { new: true, runValidators: true })
+          return await this.model.findByIdAndUpdate(reserva.id, reserva, { new: true, runValidators: true });
         }
         const nuevaReserva = new this.model(reserva); // si no existe, lo creo
         return await nuevaReserva.save();
