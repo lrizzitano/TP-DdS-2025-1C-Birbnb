0629c70d82f7b88cbee802390981e5d016969b1b
Lautaro <lautaromelchiori01@gmail.com>
2025-05-28 01:26:21 -0300

    elimino throw error para debuggear

--- backend/controllers/reservaController.js
+++ backend/controllers/reservaController.js
@@ -46,7 +46,6 @@ export class ReservaController {
       res.status(200).json(reserva);
     } catch (error) {
       res.status(500).json({ error: error.message });
-      throw error
     }
   }
 }
