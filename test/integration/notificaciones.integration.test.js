935cc27c7891fea5d69ceaf9596f55294b4f1784
Lautaro <lautaromelchiori01@gmail.com>
2025-05-28 10:22:32 -0300

    avanzo con test de integracion notif, repo mockeado - AUN NO ANDA

--- test/integration/notificaciones.integration.test.js
+++ test/integration/notificaciones.integration.test.js
@@ -3,38 +3,74 @@ import { describe, expect, jest, test } from "@jest/globals"
 import request from 'supertest'
 import { NotificacionService } from "../../backend/services/notificacionService.js"
 import { NotificacionController } from "../../backend/controllers/notificacionController.js"
+import { EstadoNotificacion } from "../../backend/modelo/enums/EstadoNotificacion"
 
 const server = buildTestServer()
 
 const mockNotificacionRepository = {
     findByDestinatario: jest.fn().mockResolvedValue([
         {
-            
+            mensaje : "NotificacionTest",
+            fechaAlta : "fechaTest",
+            estado : EstadoNotificacion.PENDIENTE,
+            fechaLeida : null,
+            destinatario : {
+                nombre: "usuarioTest"
+            }
         },
         {
-
+            mensaje : "Notificacion2est",
+            fechaAlta : "fechaTest",
+            estado : EstadoNotificacion.PENDIENTE,
+            fechaLeida : null,
+            destinatario : {
+                nombre: "usuarioTest"
+            }
         }
-    ])
-
-    async findById(id) {
-        return await this.model.findById(id);
-    }
-
-    async create(notificacion) {
-        const nuevaNotificacion = new this.model(notificacion);
-        return await nuevaNotificacion.save();
-    }
-
-    async update(notificacion) {
-        return await this.model.findByIdAndUpdate(
-        notificacion.id,
-        notificacion,
-        { new: true }
-        );
-    }    
+    ]),
+    findById: jest.fn().mockResolvedValue({
+        mensaje : "NotificacionTest",
+        fechaAlta : "fechaTest",
+        estado : EstadoNotificacion.PENDIENTE,
+        fechaLeida : null,
+        destinatario : {
+            nombre: "usuarioTest"
+        }
+    }),
+    create: jest.fn().mockResolvedValue({
+        mensaje : "NotificacionTest",
+        fechaAlta : "fechaTest",
+        estado : EstadoNotificacion.PENDIENTE,
+        fechaLeida : null,
+        destinatario : {
+            nombre: "usuarioTest"
+        }
+    }),
+    update: jest.fn().mockResolvedValue({
+        mensaje : "NotificacionTest",
+        fechaAlta : "fechaTest",
+        estado : EstadoNotificacion.PENDIENTE,
+        fechaLeida : null,
+        destinatario : {
+            nombre: "usuarioTest"
+        }
+    })  
 }
 
 const notificacionService = new ReservaService(mockNotificacionRepository)
 const notificacionController = new ReservaController(notificacionService)
 
 server.setController(notificacionController);
+
+/*
+quiero testear estas routes
+
+app.get("/notificaciones", (req, res) =>
+    getController(NotificacionController).findByDestinatario(req, res)
+);
+
+app.patch("/notificaciones/:id", (req, res) =>
+    getController(NotificacionController).updateEstado(req, res)
+);
+
+*/
