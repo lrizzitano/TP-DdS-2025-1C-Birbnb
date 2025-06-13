108d52e18e8b99704816b39502ef20a396214367
Lautaro Melchiori <lautaromelchiori01@gmail.com>
2025-06-13 13:42:48 -0300

    añado unas cards a la vista de alojamientos

--- frontend/src/pages/alojamientosPage/AlojamientosPage.js
+++ frontend/src/pages/alojamientosPage/AlojamientosPage.js
@@ -1,7 +1,20 @@
+import CardAlojamiento from "../../components/cardAlojamiento/CardAlojamiento";
+const mockAlojamiento = {fotos:["https://a0.muscache.com/im/pictures/c42d676c-4733-4151-9f40-c2903f4a3cd0.jpg?im_w=960"],
+  nombre: "Habitacion en Buenos Aires", precioPorNoche:50000, id:3
+}
 const Alojamientos = () => {
     return (
       <>
-        <p> Aca irian todos los componentes la vista Alojamientos </p>
+        <p> Aca irian todos los componentes la vista Alojamientos, pongo ejemplo de card
+            podriamos pensar como queremos mostrar todo </p>
+        <div>
+          <CardAlojamiento alojamiento={mockAlojamiento}></CardAlojamiento>
+          <CardAlojamiento alojamiento={mockAlojamiento}></CardAlojamiento>
+          <CardAlojamiento alojamiento={mockAlojamiento}></CardAlojamiento>
+          <CardAlojamiento alojamiento={mockAlojamiento}></CardAlojamiento>
+          <CardAlojamiento alojamiento={mockAlojamiento}></CardAlojamiento>
+          <CardAlojamiento alojamiento={mockAlojamiento}></CardAlojamiento>
+        </div>
       </>
     )
 };
