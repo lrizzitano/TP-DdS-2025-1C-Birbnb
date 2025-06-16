6a17eb4e137f321c07e139295a0ced089694e739
Lautaro <lautaromelchiori01@gmail.com>
2025-06-16 18:52:54 -0300

    agrego funcion para llamar a la api del backend para obtener alojamientos

---
+++ frontend/src/api/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "localhost:3000";
+
+export const getAlojamientos = async (filtros = {}) => {
+    /*
+    el objeto filtro debe tener las keys nombradas asi:
+        pais
+        lat
+        long
+        precioMin
+        precioMax
+        cantHuespede
+        caracteristicas
+        page
+        limit
+    */
+  try {
+    const response = await axios.get(`${API_BASE_URL}/products`, {
+      params: filtros
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    throw error;
+  }
+};
