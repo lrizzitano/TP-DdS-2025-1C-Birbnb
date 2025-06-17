1461b6c0b1e8fa2f16b8bc50d36d723fce06286b
Lautaro <lautaromelchiori01@gmail.com>
2025-06-17 17:37:34 -0300

    hago responsive la vista de los alojamientos con sus filtros

--- frontend/src/pages/alojamientosPage/AlojamientosPage.js
+++ frontend/src/pages/alojamientosPage/AlojamientosPage.js
@@ -3,10 +3,16 @@ import { useState, useEffect } from "react";
 import CardAlojamiento from "../../components/cardAlojamiento/CardAlojamiento";
 import { fetchAlojamientosBackend } from "../../api/api";
 import FiltrosAlojamientos from "../../components/filtrosAlojamientos/FiltrosAlojamientos";
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { IconButton } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const Alojamientos = () => {
   const [alojamientos, setAlojamientos] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [filtersOut, setFiltersOut] = useState(true);
+  const matches = useMediaQuery('(min-width: 768px)')
+
 
   // los temporales serian los que estan seleccionados pero no aplicados (se aplican al tocar el boton buscar)
   const [filtrosTemporales, setFiltrosTemporales] = useState({});
@@ -28,11 +34,18 @@ const Alojamientos = () => {
 
   return (
       <div className = "pageAlojamientos">
-          <FiltrosAlojamientos
-            filtrosTemporales={filtrosTemporales}
-            setFiltrosTemporales={setFiltrosTemporales}
-            onBuscar={handleBuscar}
-          />
+      <IconButton onClick={() => {setFiltersOut(!filtersOut)}} color="inherit" sx={{display: 'none', '@media (max-width: 768px)': { display: 'flex' }}}>
+          <FilterListIcon />
+      </IconButton>
+
+      {(filtersOut || matches) && <div className={"contenedorFiltros"}>
+        <FiltrosAlojamientos
+          filtrosTemporales={filtrosTemporales}
+          setFiltrosTemporales={setFiltrosTemporales}
+          onBuscar={handleBuscar}
+        />
+      </div>}
+
         {loading ? (
           <p>Cargando alojamientos...</p>
         ) : (
