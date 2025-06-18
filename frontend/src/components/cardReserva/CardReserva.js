d4b252dbe94b4c4d2feba97b7bbdd545cc6a3821
lrizzitano <lrizzitano@frba.utn.edu.ar>
2025-06-18 01:25:22 -0300

    mejoro maqueta de vista de huesped

--- frontend/src/components/cardReserva/CardReserva.js
+++ frontend/src/components/cardReserva/CardReserva.js
@@ -1,33 +1,38 @@
 import { Card, CardContent, Typography } from '@mui/material';
+import './CardReserva.css'; 
 
 const CardReserva = ({ reserva }) => {
-  const fechaInicio = new Date(reserva.rangoFechas.fechaInicio).toLocaleDateString();
-  const fechaFin = new Date(reserva.rangoFechas.fechaFin).toLocaleDateString();
-  const fechaAlta = new Date(reserva.fechaAlta).toLocaleDateString();
+    const fechaInicio = new Date(reserva.rangoFechas.fechaInicio).toLocaleDateString();
+    const fechaFin = new Date(reserva.rangoFechas.fechaFin).toLocaleDateString();
+    const fechaAlta = new Date(reserva.fechaAlta).toLocaleDateString();
 
-  return (
-    <Card variant="outlined" sx={{ mb: 2 }}>
-      <CardContent>
-        <Typography variant="h6" gutterBottom>
-          Reserva #{reserva.id.slice(0, 6).toUpperCase()}
-        </Typography>
-        <Typography variant="body2">
-          Estado: <strong>{reserva.estado}</strong>
-        </Typography>
-        <Typography variant="body2">
-          Fecha de alta: {fechaAlta}
-        </Typography>
-        <Typography variant="body2">
-          Rango de fechas: {fechaInicio} - {fechaFin}
-        </Typography>
-        <Typography variant="body2">
-          Cantidad de huéspedes: {reserva.cantidadHuespedes}
-        </Typography>
-        <Typography variant="body2">
-          Precio por noche: ${reserva.precioPorNoche}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card variant="outlined" sx={{ mb: 2 }}>
+            <CardContent className="contenedorReserva">
+
+                <div className="infoReserva">
+                    <Typography variant="h6" gutterBottom>
+                        Reserva #{reserva.id.slice(0, 6).toUpperCase()}
+                    </Typography>
+                    <Typography variant="body2">Estado: <strong>{reserva.estado}</strong></Typography>
+                    <Typography variant="body2">Fecha de alta: {fechaAlta}</Typography>
+                    <Typography variant="body2">Rango de fechas: {fechaInicio} - {fechaFin}</Typography>
+                    <Typography variant="body2">Cantidad de huéspedes: {reserva.cantidadHuespedes}</Typography>
+                    <Typography variant="body2">Precio por noche: ${reserva.precioPorNoche}</Typography>
+                </div>
+
+                <div className="infoAlojamiento">
+                    <img
+                        src={reserva.alojamiento.fotos[0].path}
+                        alt={reserva.alojamiento.fotos[0].descripcion}
+                        className="imagenAlojamiento"
+                    />
+                    <Typography variant="body1" className="nombreAlojamiento">
+                        <strong>{reserva.alojamiento.nombre}</strong>
+                    </Typography>
+                </div>
+            </CardContent>
+        </Card>
+    );
 }
 export default CardReserva;
--- frontend/src/pages/huespedPage/HuespedPage.js
+++ frontend/src/pages/huespedPage/HuespedPage.js
@@ -10,7 +10,16 @@ const reservasEjemplo = [
     fechaAlta: "Wed May 28 2025 16:32:44 GMT-0300 (Argentina Standard Time)",
     huespedReservadorId: "68367739102a6bf29a3d1ef0",
     cantidadHuespedes: 4,
-    alojamientoId: "6835f4cfe3a15cf7e8ffa806",
+    alojamiento: {
+      id: "6835f4cfe3a15cf7e8ffa805",
+      nombre: "Casa en la Playa",
+      fotos: [
+        {
+          "descripcion": "Vista al mar desde el balcón",
+          "path": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.adsttc.com%2Fmedia%2Fimages%2F54ee%2Fabf8%2Fe58e%2Fce7e%2F1000%2F0060%2Flarge_jpg%2FE03.jpg%3F1424927721&f=1&nofb=1&ipt=acbed40e3d8e55c9ba275085f73aa383bc40baccc62a1c7db9c98cca845e8998"
+        }
+      ]
+    },
     rangoFechas: {
       fechaInicio: "Sat Mar 31 2029 21:00:00 GMT-0300 (Argentina Standard Time)",
       fechaFin: "Wed Apr 04 2029 21:00:00 GMT-0300 (Argentina Standard Time)"
@@ -24,7 +33,16 @@ const reservasEjemplo = [
     fechaAlta: "Wed May 28 2025 18:40:37 GMT-0300 (Argentina Standard Time)",
     huespedReservadorId: "68367739102a6bf29a3d1ef0",
     cantidadHuespedes: 3,
-    alojamientoId: "6835f4cfe3a15cf7e8ffa806",
+    alojamiento: {
+      nombre: "Cabaña en la Montaña",
+      fotos: [
+        {
+          "descripcion": "Exterior nevado en invierno",
+          "path": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.decoist.com%2Fwp-content%2Fuploads%2F2017%2F05%2FDark-wooden-cottage-stands-out-among-the-green-and-gray-backdrop-.jpeg&f=1&nofb=1&ipt=1594285dda797232b32e4743376a39faa5c74454cee38a1ff8b3ac27fe6e89b6"
+        }
+      ]
+
+    },
     rangoFechas: {
       fechaInicio: "Sat Mar 31 2029 21:00:00 GMT-0300 (Argentina Standard Time)",
       fechaFin: "Wed Apr 04 2029 21:00:00 GMT-0300 (Argentina Standard Time)"
