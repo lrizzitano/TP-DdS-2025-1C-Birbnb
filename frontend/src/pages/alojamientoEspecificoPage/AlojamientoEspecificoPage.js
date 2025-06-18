f9938cc9afa220659d11c83de5a6e24d5d61f8e7
Lautaro <lautaromelchiori01@gmail.com>
2025-06-18 03:41:29 -0300

    conecto la vista de alojamiento especifico al backend

--- frontend/src/pages/alojamientoEspecificoPage/AlojamientoEspecificoPage.js
+++ frontend/src/pages/alojamientoEspecificoPage/AlojamientoEspecificoPage.js
@@ -1,29 +1,39 @@
 import { useParams } from "react-router-dom";
-
+import { fetchAlojamiento } from "../../api/api";
+import { useState, useEffect } from "react";
+import './AlojamientoEspecificoPage.css'
 const AlojamientoEspecificoPage = () => {
     const { id } = useParams(); //sacamos el id de la url (parte de react router, lo saque de clases sabado)
-    const alojamiento = getAlojamiento(id) // aca podriamos ir a buscarlo al backend
+
+    const [alojamiento, setAlojamiento] = useState(0);
+    const [loading, setLoading] = useState(true);
+
+     useEffect(() => {
+        setLoading(true);
+        fetchAlojamiento(id).then(a => {
+          setAlojamiento(a);
+        })
+          .then(() => setLoading(false))
+          .catch(() => setLoading(false))
+      }, [id]);
 
     // seria la verificacion de no encontrarlo
     if (!alojamiento) return <div>Producto no encontrado</div>;
 
     return (
-      <>
-        <p> Aca irian todos los componentes la vista alojamiento especifico </p>
-        {/* Esto probablemente seria interesante hacerlo con componentes */}
-        <img src={alojamiento.fotos[0]} width={"50%"} alt={alojamiento.nombre}></img>
-        <h2>{alojamiento.nombre}</h2>
-        <h3>Acerca de este lugar</h3>
-        <p>{alojamiento.descripcion}</p>
-      </>
+    <>
+      {loading ? (
+        <p>Cargando alojamientos...</p>
+      ) : (
+        <div className = "contenedorAlojamiento">
+          <h2>{alojamiento.nombre}</h2>
+          <img src={alojamiento.fotos[0].path} width={"50%"} alt={alojamiento.nombre}></img>
+          <h3>Acerca de este lugar</h3>
+          <p>{alojamiento.descripcion}</p>
+        </div>
+      )}
+    </>
     )
 };
-
-// funcion truchita para ir probando cosas
-const getAlojamiento = (id) => {
-  return {fotos:["https://a0.muscache.com/im/pictures/c42d676c-4733-4151-9f40-c2903f4a3cd0.jpg?im_w=960"],
-  nombre: "Habitacion en Buenos Aires", precioPorNoche:50000, id:3,
-  descripcion: `Habitación individual en un departamento luminoso y espacioso en el último piso (tercer piso y sin ascensor). La habitación es pequeña en tamaño y es de 3,6 m x 2 m = 7,2 metros cuadrados.Hay muchas tiendas y lugares para comer cerca, así como excelentes conexiones de transporte público (estaciones de metro West Kensington y Barons Court a menos de 2 a 7 minutos, y 28/306 rutas de autobús).Acceso a la terraza privada en la azotea disponible.`
-}}
 
 export default AlojamientoEspecificoPage; 
