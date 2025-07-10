be301bc3f9ed179a9ff5a54fd5fce6315debe3f2
Lautaro <lautaromelchiori01@gmail.com>
2025-07-10 16:50:55 -0300

    termino test e2e

--- frontend/cypress/e2e/crearReserva.cy.js
+++ frontend/cypress/e2e/crearReserva.cy.js
@@ -2,8 +2,10 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
 
-    cy.get('.contenedorCardsAlojamiento > div img').should('be.visible');
+    // cambiando el div:nth-child(n) se hace la prueba completa sobre otro alojamiento
+    cy.get('.contenedorCardsAlojamiento > div:nth-child(1) img').should('be.visible');
 
+    // continuo el test dentro de then pq la busqueda del texto es asincrona y los demas chequeos dependen de ese texto
     cy.get('.contenedorCardsAlojamiento > div:nth-child(1) a > div > div').invoke('text').then((textoExtraido) => {
       let nombreAlojamiento = textoExtraido.trim();
 
@@ -12,12 +14,24 @@ describe('template spec', () => {
       cy.get('.contenedorCardsAlojamiento > div:nth-child(1) a')
       .invoke('removeAttr', 'target') // para que no se abra en otra tab, asi podemos testear
       .click(); // nos vamos al alojamiento especifico
-  
-  
+
       cy.get('.contenedorAlojamiento .tituloAlojamiento h4').should('contain', nombreAlojamiento);
       cy.get('.contenedorAlojamiento .dataAlojamiento img').should('be.visible')
       
-      cy.get('.contenedorAlojamiento .dataAlojamiento a').click()
+      cy.contains('a', 'Reservar')
+      .click()
+
+      cy.contains('button', 'Confirmar Reserva')
+      .click();
+
+      cy.get('[role="dialog"]') // agarro el modal de MUI
+      .contains('Reserva confirmada');
+
+      cy.contains('button', 'Ir a mis reservas').click()
+
+      cy.get('[data-testid="ExpandMoreIcon"]').first().click();
+
+      cy.get('.reservasContainer').first().find('div').first().find('p').contains(nombreAlojamiento)
     });
   })
 })
